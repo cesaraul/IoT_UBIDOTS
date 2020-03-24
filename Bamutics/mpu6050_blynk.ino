5d567ca70fa8a8fab361ea026d95fb01831f035d
Cesar Cruz <cesaraulcg@gmail.com>
2020-03-24 12:01:33 -0500

    girscopio ascelerometro

---
+++ Bamutics/mpu6050_blynk.ino
@@ -0,0 +1,48 @@
+
+#define BLYNK_PRINT Serial
+
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "8RQIXG_Mmjqs5y76ycaJNxBF4we7z7ZL";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "iPhone de Jean Pieer";
+char pass[] = "12345678";
+
+BlynkTimer timer;
+
+// This function sends Arduino's up time every second to Virtual Pin (5).
+// In the app, Widget's reading frequency should be set to PUSH. This means
+// that you define how often to send data to Blynk App.
+void myTimerEvent()
+{
+  // You can send any value at any time.
+  // Please don't send more that 10 values per second.
+  int analog=analogRead(A0);
+  Blynk.virtualWrite(analog / 1000);
+}
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+
+  Blynk.begin(auth, ssid, pass);
+  // You can also specify server:
+  //Blynk.begin(auth, ssid, pass, "blynk-cloud.com", 80);
+  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
+
+  // Setup a function to be called every second
+  timer.setInterval(1000L, myTimerEvent);
+}
+
+void loop()
+{
+  Blynk.run();
+  timer.run(); // Initiates BlynkTimer
+}
